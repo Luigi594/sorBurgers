f110418a5738a4977bcd1d46de7f0f2e46e558ef
= <=>
2022-02-25 20:54:34 -0600

    Ya inserta en usuarios

--- src/routes/rutasUsuarios.js
+++ src/routes/rutasUsuarios.js
@@ -6,7 +6,6 @@ const controladorUsuario = require('../controllers/controllerUsuario');
 
 router.get('/', controladorUsuario.ListaUsuariosClientes);
 router.get('/empleados', controladorUsuario.ListaUsuariosEmpleados);
-router.get('/lista/clientes', controladorUsuario.ObtenerClienteRegistrado);
 
 // este es para guardar el usuario del cliente
 router.post('/guardar/cliente', controladorUsuario.GuardarUsuarioCliente);
