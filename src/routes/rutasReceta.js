4f8d8481676491dd1d66ca13540870dc30654cd2
EduardoAguirre2021 <aguirreeduardo02@gmail.com>
2022-02-27 20:54:33 -0600

    Crud Recetas, agregado de validaciones

--- src/routes/rutasReceta.js
+++ src/routes/rutasReceta.js
@@ -1,14 +1,14 @@
 const {Router} = require('express');
 const router = Router();
 const { body } = require('express-validator');
-//const validaciones = require('../middleware/authMiddleware');
+const validaciones = require('../middleware/authReceta');
 
 // importando el controlador
 const controladorReceta = require('../controllers/controllerReceta');
 
 router.get('/', controladorReceta.ListaRecetas);
-router.post('/guardar', controladorReceta.GuardaReceta);
-router.put('/modificar', controladorReceta.ModificarReceta);
+router.post('/guardar', validaciones.Validacion, controladorReceta.GuardaReceta);
+router.put('/modificar', validaciones.Validacion, controladorReceta.ModificarReceta);
 router.delete('/eliminar', controladorReceta.EliminarReceta);
 
 module.exports = router;
